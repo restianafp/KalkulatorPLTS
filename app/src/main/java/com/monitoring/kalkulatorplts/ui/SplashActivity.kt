f5cb772cde81058f2ff4e723503c7bf1388133a8
restianafp <restianafp@gmail.com>
2021-10-21 12:49:43 +0700

    setup style and theme

--- app/src/main/java/com/monitoring/kalkulatorplts/ui/SplashActivity.kt
+++ app/src/main/java/com/monitoring/kalkulatorplts/ui/SplashActivity.kt
@@ -1,9 +1,10 @@
-package com.monitoring.kalkulatorplts
+package com.monitoring.kalkulatorplts.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.monitoring.kalkulatorplts.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
