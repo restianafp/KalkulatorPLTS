21f83680234d2f0bd2cfaf73dc6e19f80a5c8ab5
restianafp <restianafp@gmail.com>
2021-10-26 12:16:18 +0700

    add activity

---
+++ app/src/main/java/com/monitoring/kalkulatorplts/app/ui/simple/HasilSimpleAC.kt
@@ -0,0 +1,12 @@
+package com.monitoring.kalkulatorplts.app.ui.simple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.monitoring.kalkulatorplts.R
+
+class HasilSimpleAC : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hasil_simple)
+    }
+}
---
+++ app/src/main/java/com/monitoring/kalkulatorplts/app/ui/splash/GetStarted.kt
@@ -0,0 +1,12 @@
+package com.monitoring.kalkulatorplts.app.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.monitoring.kalkulatorplts.R
+
+class GetStarted : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_started)
+    }
+}
