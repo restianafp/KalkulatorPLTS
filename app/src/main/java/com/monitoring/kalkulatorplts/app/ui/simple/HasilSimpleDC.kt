614db4ea58c84dc122eec5ffe01e259bec48fbce
restianafp <restianafp@gmail.com>
2021-10-28 10:40:55 +0700

    slicing hasil hitun dc simple

---
+++ app/src/main/java/com/monitoring/kalkulatorplts/app/ui/simple/HasilSimpleDC.kt
@@ -0,0 +1,12 @@
+package com.monitoring.kalkulatorplts.app.ui.simple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.monitoring.kalkulatorplts.R
+
+class HasilSimpleDC : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hasil_simple_dc)
+    }
+}
